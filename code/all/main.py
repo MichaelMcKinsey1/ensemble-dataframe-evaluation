ee2e2577cb3f8d3f3abc889da504288ca39ab816
Michael McKinsey <MichaelMcKinsey1@gmail.com>
2022-02-27 18:33:36 -0600

    na

--- code/all/main.py
+++ code/all/main.py
@@ -9,12 +9,18 @@ import hatchet as ht
 import lhue.hatchet.util.unify_ensemble as unify_ensemble
 
 graph_frame_list = []
-dataset = "../../data/lulesh-scaling/lulesh-annotation-profile-512cores.json"
+dataset = "../../data/lulesh-scaling/lulesh-annotation-profile-1core.json"
 num_datasets = 10 # How many times to replicate dataset
+
+gf2 = ht.GraphFrame.from_caliper_json(dataset)
+gf2.dataframe.reset_index("rank", drop=True, inplace=True)
+print(gf2.dataframe.index.names)
+display(gf2.dataframe)
 
 composition_time_start = time.time()
 for i in range(num_datasets): # https://github.com/TauferLab/llnl-hatchet/blob/bd7d4c96c7a20d7a7b05cf1a4f41846ae36ab980/hatchet/tests/graphframe.py#L1186-L1192
     gf = ht.GraphFrame.from_caliper_json(dataset)
+    gf.dataframe.reset_index("rank", drop=True, inplace=True)
     dset_name = "dset{}".format(i)
     gf.dataset = dset_name
     graph_frame_list.append(gf)
@@ -48,7 +54,7 @@ print(f"Composition time (Read time + Unify time) for {num_datasets} datasets ({
 dask_dataframe_size = sys.getsizeof(computed_ddf)
 print("Size of dask dataframe: " + str(dask_dataframe_size) + " bytes. " + str(dask_dataframe_size/1000000) + " mb(s).")
 
-
+quit()
 
 ### Arrow ###
 print("\n\n############ ARROW ############")
