913356043e4998523bc0f266ae724d67e9a8977a
Michael Richard McKinsey <mckinsey@catalyst113.llnl.gov>
2022-04-29 14:42:13 -0700

    Add statistics for slices of Pandas dataframes

--- code/main/code/main.py
+++ code/main/code/main.py
@@ -5,13 +5,14 @@ import pyarrow as pa
 import numpy as np
 import helpers
 from evalu import pandas_ev, dask_ev, arrow_ev
+from slicing import slice_tests
 
-EXTRA_OUT=True
+EXTRA_OUT=False
 MEMORY=True
 TIME=True
 BUILT_IN=True
 NANOS=1000000000
-N_TRAVERSALS=1000
+N_TRAVERSALS=100
 X=100
 
 # Pandas display options for cli.
@@ -21,38 +22,49 @@ pd.set_option('display.max_columns', 10)
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
 
+# def analyze_object_memory(?):
+#     # # Memory Analysis of single objects.
+#     # computed_ddf = dask_df.compute()
+#     # print(pandas_df_reindexed['node'].iloc[0])
+#     # print(computed_ddf['node'].iloc[0])
+#     # for i in range(len(pandas_df_reindexed)):
+#     #     if str(pandas_df_reindexed['node'].iloc[i]) == str(computed_ddf['node'].iloc[i]):
+#     #         print(f"{i}: {str(pandas_df_reindexed['node'].iloc[i])} == {str(computed_ddf['node'].iloc[i])}")
+#     #         print(f"{i}: {sys.getsizeof(pandas_df_reindexed['node'].iloc[i])} == {sys.getsizeof(computed_ddf['node'].iloc[i])}")
+#     #         print(f"{i}: {type(pandas_df_reindexed['node'].iloc[i])} == {type(computed_ddf['node'].iloc[i])}")
+
 def main():
     start_total_time = time.time()
 
     dataset = str(sys.argv[1])
     reader_type = str(sys.argv[2])
     
-    pandas_df_from_hatchet, pandas_df_original = (helpers.prep_hatchet_gf(dataset, reader_type)[0]).dataframe, (helpers.prep_hatchet_gf(dataset, reader_type)[1]).dataframe # Hatchet graphframe.dataframe
-    dask_df =  helpers.prep_dask_df(pandas_df_from_hatchet) # Pre-computed dask dataframe
-    arrow_table = helpers.prep_arrow_table(pandas_df_from_hatchet) # Arrow table from pandas.
-
-    # computed_ddf = dask_df.compute()
-    # print(pandas_df_from_hatchet['node'].iloc[0])
-    # print(computed_ddf['node'].iloc[0])
-    # for i in range(len(pandas_df_from_hatchet)):
-    #     if str(pandas_df_from_hatchet['node'].iloc[i]) == str(computed_ddf['node'].iloc[i]):
-    #         print(f"{i}: {str(pandas_df_from_hatchet['node'].iloc[i])} == {str(computed_ddf['node'].iloc[i])}")
-    #         print(f"{i}: {sys.getsizeof(pandas_df_from_hatchet['node'].iloc[i])} == {sys.getsizeof(computed_ddf['node'].iloc[i])}")
-    #         print(f"{i}: {type(pandas_df_from_hatchet['node'].iloc[i])} == {type(computed_ddf['node'].iloc[i])}")
+    pandas_df_reindexed, pandas_df_original = (helpers.prep_hatchet_gf(dataset, reader_type)[0]).dataframe, (helpers.prep_hatchet_gf(dataset, reader_type)[1]).dataframe # Hatchet graphframe.dataframe
+    dask_df =  helpers.prep_dask_df(pandas_df_reindexed) # Pre-computed dask dataframe
+    arrow_table = helpers.prep_arrow_table(pandas_df_reindexed) # Arrow table from pandas.
+    
+    pandas_orig_unstacked_zero = pandas_df_original.unstack(level=0)
+    pandas_orig_unstacked_one = pandas_df_original.unstack(level=1)
 
     print(f"Rows: {len(pandas_df_original)}")
     print(f"Columns: {len(pandas_df_original.columns)}\n")
 
-    for i in range(2):
-        print(f"level={i}")
-        print(pandas_df_original.unstack(level=i))
-        print("\n")
+    helpers.print_separator("Slicing")
+    slice_tests.slicing_tests(pandas_df_original, 10, X, N_TRAVERSALS, EXTRA_OUT)
+
+    exit()
+
+    helpers.print_separator("Pandas Original (unstacked 0)")
+    pandas_zero_time, pandas_zero_built = pandas_ev.pandas_eval(pandas_orig_unstacked_zero, EXTRA_OUT, MEMORY, TIME, BUILT_IN, N_TRAVERSALS)
+
+    helpers.print_separator("Pandas Original (unstacked 1)")
+    pandas_one_time, pandas_one_built = pandas_ev.pandas_eval(pandas_orig_unstacked_one, EXTRA_OUT, MEMORY, TIME, BUILT_IN, N_TRAVERSALS)
 
     helpers.print_separator("Pandas Original")
     pandas_orig_time, pandas_orig_built = pandas_ev.pandas_eval(pandas_df_original, EXTRA_OUT, MEMORY, TIME, BUILT_IN, N_TRAVERSALS)
 
-    helpers.print_separator("Pandas")
-    pandas_time, pandas_built = pandas_ev.pandas_eval(pandas_df_from_hatchet, EXTRA_OUT, MEMORY, TIME, BUILT_IN, N_TRAVERSALS)
+    # helpers.print_separator("Pandas Re-indexed")
+    # pandas_time, pandas_built = pandas_ev.pandas_eval(pandas_df_reindexed, EXTRA_OUT, MEMORY, TIME, BUILT_IN, N_TRAVERSALS)
 
     # helpers.print_separator("Dask")
     # dask_time, dask_built = dask_ev.dask_eval(dask_df, EXTRA_OUT, MEMORY, TIME, BUILT_IN, N_TRAVERSALS)
@@ -61,9 +73,13 @@ def main():
     # arrow_time, arrow_built = arrow_ev.arrow_eval(arrow_table, EXTRA_OUT, MEMORY, TIME, BUILT_IN, N_TRAVERSALS)
 
     helpers.print_separator("Results")
-    eval_list=['Pandas Original', 'Pandas']#, 'Dask', 'Arrow']
-    time_data = [pandas_orig_time, pandas_time]#, dask_time, arrow_time]
-    built_data = [pandas_orig_built, pandas_built]#, dask_built, arrow_built]
+    print(f"*Entries are relative time measurements per-{N_TRAVERSALS} element accesses. X={X}")
+    eval_list=[f'Pandas Original (unstacked 0) ({len(pandas_orig_unstacked_zero)}x{len(pandas_orig_unstacked_zero.columns)})',
+        f'Pandas Original (unstacked 1) ({len(pandas_orig_unstacked_one)}x{len(pandas_orig_unstacked_one.columns)})', 
+        f'Pandas Original ({len(pandas_df_original)}x{len(pandas_df_original.columns)})'] 
+        #f'Pandas Re-indexed ({len(pandas_df_reindexed)}x{len(pandas_df_reindexed.columns)})', 'Dask', 'Arrow']
+    time_data = [pandas_zero_time, pandas_one_time, pandas_orig_time]#, pandas_time, dask_time, arrow_time]
+    built_data = [pandas_zero_built, pandas_one_built, pandas_orig_built]#, pandas_built, dask_built, arrow_built]
     X_lambda_func = lambda x: f"{round(x/X)}*X" if (not np.isnan(x)) else x
     res_df_time = pd.DataFrame(data=time_data, index=eval_list)
     res_df_time = res_df_time.applymap(X_lambda_func)
@@ -71,9 +87,10 @@ def main():
     res_df_built = pd.DataFrame(data=built_data, index=eval_list)
     res_df_built = res_df_built.applymap(X_lambda_func)
     print(res_df_built)
-
+    
     end_total_time = time.time() - start_total_time
     print(f"\n\n\nTotal Program Time: {end_total_time}s. {end_total_time/60}m.")
 
 
-main()
+if __name__ == "__main__":
+    main()
---
+++ code/main/code/slicing/slice_tests.py
@@ -0,0 +1,117 @@
+from scipy import stats
+import copy
+import time
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+'''
+Info:
+    - Python slicing
+        Example: a[start:stop:step] # start through not past stop, by step (https://stackoverflow.com/questions/509211/understanding-slicing)
+    - Label indexing (with hatchet node as zero index)
+        hatchet_node = hatchet.node.Node({'name': '<program root>', 'type': 'function'})
+        print(hatchet_node)
+        print(type(hatchet_node))
+        print(df[hatchet_node, 0]) # <- Does not work :(
+    - location indexing
+        print(df.iloc[0::step_size]) # Get every node's rank 0
+        print(df.iloc[1::step_size]) # Get every node's rank 1
+        print(df.index.levels[0]) # node
+        print(df.index.levels[1]) # rank
+'''
+
+
+def slice_statistics(a_slice, nobs, N_TRAVERSALS):
+    '''Computes statistics on a slice (pandas.core.series.Series). Should be a numeric slice so sum can be computed.
+
+    Args:
+        a_slice: Slice of pandas dataframe.
+        nobs: Number of observations.
+
+    Returns:
+        ?
+    '''
+    num_rows = len(a_slice)
+    deep_slice = copy.deepcopy(a_slice)
+    t_arr = []
+    dt_arr = []
+    bt_arr = []
+    bdt_arr = []
+
+    # traversals
+    for i in range(nobs):
+        start = time.time_ns()
+        for index, value in a_slice.iteritems():
+            value
+        end = (time.time_ns() - start)
+        adj_metric = (end*N_TRAVERSALS)/(num_rows)
+        t_arr.append(adj_metric)
+
+        start = time.time_ns()
+        for index, value in deep_slice.iteritems():
+            value
+        end = (time.time_ns() - start)
+        adj_metric = (end*N_TRAVERSALS)/(num_rows)
+        dt_arr.append(adj_metric)
+        
+    
+    # built-in
+    for i in range(nobs):
+        start = time.time_ns()
+        s_sum = a_slice.sum()
+        end = time.time_ns() - start
+        adj_metric = (end*N_TRAVERSALS)/(num_rows)
+        bt_arr.append(adj_metric)
+
+        start = time.time_ns()
+        ds_sum = deep_slice.sum()
+        end = time.time_ns() - start
+        adj_metric = (end*N_TRAVERSALS)/(num_rows)
+        bdt_arr.append(adj_metric)
+
+        assert(s_sum==ds_sum)
+        
+    traversal_stats = stats.describe(t_arr)
+    dtraversal_stats = stats.describe(dt_arr)
+    built_stats = stats.describe(bt_arr)
+    dbuilt_stats = stats.describe(bdt_arr)
+
+    return traversal_stats, dtraversal_stats, built_stats, dbuilt_stats
+
+
+def slicing_tests(df, nobs, X, N_TRAVERSALS, EXTRA_OUT):
+    '''
+    Args:
+        df: A 2-level multi-indexed pandas dataframe.
+        nobs: Number of observations.
+    '''
+    step_size = df.index.levels[1].size # number of elements in the second level (level 0).
+
+    rank_zero_times = df.iloc[0::step_size]['time']
+    rzt_stats, drzt_stats, rzt_built_stats, drzt_built_stats = slice_statistics(rank_zero_times, nobs, N_TRAVERSALS)
+
+    rank_one_times = df.iloc[1::step_size]['time']
+    rot_stats, drot_stats, rot_built_stats, drot_built_stats = slice_statistics(rank_one_times, nobs, N_TRAVERSALS)
+
+    if EXTRA_OUT:
+        print(rank_zero_times)
+        print(rank_one_times)
+        stats_list = [rzt_stats, drzt_stats, rzt_built_stats, drzt_built_stats,
+            rot_stats, drot_stats, rot_built_stats, drot_built_stats]
+        for i in stats_list:
+            print(i)
+    
+    print(f"*Entries are relative time measurements per-{N_TRAVERSALS} element accesses. X={X}")
+    eval_list=[f'Nodes=all, Rank=0 ({len(rank_zero_times)}x1)',
+        f'Nodes=all, Rank=1 ({len(rank_one_times)}x1)']
+    time_data = {"Slice": [rzt_stats, rot_stats], "Deep Copy": [drzt_stats, drot_stats]}
+    built_data = {"Slice": [rzt_built_stats, rot_built_stats], "Deep Copy": [drzt_built_stats, drot_built_stats]}
+    X_lambda_func = lambda x: f"{round(x.mean/X)}*X" if (not np.isnan(x.mean)) else x.mean
+    df_time = pd.DataFrame(data=time_data, index=eval_list)
+    df_time = df_time.applymap(X_lambda_func)
+    print(f"Traversal Times\n{df_time}")
+    df_built = pd.DataFrame(data=built_data, index=eval_list)
+    df_built = df_built.applymap(X_lambda_func)
+    print(f"Built-in (Sum)\n{df_built}")
+    
